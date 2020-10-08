d133425db342bb955ed9d8caae5f9fa6f1865c6d
Piotr Jura <>
2020-10-08 20:09:14 +0200

    #0008 Route param constraints

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,6 +47,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        Route::pattern('id', '[0-9]+');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,11 @@ Route::get('/contact', function () {
 
 Route::get('/posts/{id}', function ($id) {
     return 'Blog post ' . $id;
-})->name('posts.show');
+})
+    // ->where([
+    //     'id' => '[0-9]+'
+    // ])
+    ->name('posts.show');
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Posts from ' . $daysAgo . ' days ago';
