fee341d2445484855fea6288c3dc402a10561ac3
Piotr Jura <>
2020-10-08 19:53:16 +0200

    #0007 Optional routes

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ Route::get('/contact', function () {
 
 Route::get('/posts/{id}', function ($id) {
     return 'Blog post ' . $id;
-});
+})->name('posts.show');
+
+Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
+    return 'Posts from ' . $daysAgo . ' days ago';
+})->name('posts.recent.index');
