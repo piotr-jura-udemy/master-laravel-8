d08932a715c427bc7d1016bd51a8ac37350bc598
Piotr Jura <>
2020-10-11 20:05:50 +0200

    #0013 Conditional rendering with @if/@elseif/@else

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,11 @@
 @section('title', $post['title'])
 
 @section('content')
+@if($post['is_new'])
+<div>A new blog post! Using if</div>
+@else
+<div>Blog post is old! using elseif/else</div>
+@endif
 <h1>{{ $post['title'] }}</h1>
 <p>{{ $post['content'] }}</p>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,13 @@ Route::get('/posts/{id}', function ($id) {
   $posts = [
     1 => [
       'title' => 'Intro to Laravel',
-      'content' => 'This is a short intro to Laravel'
+      'content' => 'This is a short intro to Laravel',
+      'is_new' => true
     ],
     2 => [
       'title' => 'Intro to PHP',
-      'content' => 'This is a short intro to PHP'
+      'content' => 'This is a short intro to PHP',
+      'is_new' => false
     ]
   ];
 
