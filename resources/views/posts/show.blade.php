3e12f7a7ea9b264511fd01005726b668ee289094
Piotr Jura <>
2020-10-12 17:17:55 +0200

    #0014 Conditional rendering alternatives @unless, @isset

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,16 @@
 @else
 <div>Blog post is old! using elseif/else</div>
 @endif
+
+@unless($post['is_new'])
+<div>It is an old post... using unless</div>
+@endunless
+
 <h1>{{ $post['title'] }}</h1>
 <p>{{ $post['content'] }}</p>
+
+@isset($post['has_comments'])
+<div>The post has some comments... using isset</div>
+@endisset
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,8 @@ Route::get('/posts/{id}', function ($id) {
     1 => [
       'title' => 'Intro to Laravel',
       'content' => 'This is a short intro to Laravel',
-      'is_new' => true
+      'is_new' => true,
+      'has_comments' => true
     ],
     2 => [
       'title' => 'Intro to PHP',
