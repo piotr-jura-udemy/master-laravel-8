bd746793bfe323f00cc07bcb88d791647f93fb99
Piotr Jura <>
2020-10-18 16:09:17 +0200

    #0019 Response codes, headers, cookies

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,9 @@ Route::get('/posts/{id}', function ($id) use ($posts) {
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
   return 'Posts from ' . $daysAgo . ' days ago';
 })->name('posts.recent.index');
+
+Route::get('/fun/responses', function() use($posts) {
+  return response($posts, 201)
+    ->header('Content-Type', 'application/json')
+    ->cookie('MY_COOKIE', 'Piotr Jura', 3600);
+});
