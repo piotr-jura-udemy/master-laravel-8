562d9100865d8f3e9e75a50f4a3a2c9d874d9193
Piotr Jura <>
2020-11-04 19:55:37 +0100

    #0045 Forms data validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -35,6 +35,10 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'bail|required|min:5|max:100',
+            'content' => 'required|min:10'
+        ]);
         $post = new BlogPost();
         $post->title = $request->input('title');
         $post->content = $request->input('content');
