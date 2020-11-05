5dcbf2d500ec80008e64c6e0c533ad24d767d1c6
Piotr Jura <>
2020-11-05 16:55:31 +0100

    #0046 Forms displaying errors

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,7 +6,19 @@
 <form action="{{ route('posts.store') }}" method="POST">
     @csrf
     <div><input type="text" name="title"></div>
+    @error('title')
+        <div>{{ $message }}</div>
+    @enderror
     <div><textarea name="content"></textarea></div>
+    @if($errors->any())
+        <div>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div><input type="submit" value="Create"></div>
 </form>
 @endsection
