624ac3d2b735ed18974a18c0f90ad500182f45d6
Piotr Jura <>
2020-11-09 19:02:26 +0100

    #0052 CRUD - update action

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -75,9 +75,16 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StorePost $request, $id)
     {
-        //
+        $post = BlogPost::findOrFail($id);
+        $validated = $request->validated();
+        $post->fill($validated);
+        $post->save();
+
+        $request->session()->flash('status', 'Blog post was updated!');
+
+        return redirect()->route('posts.show', ['post' => $post->id]);
     }
 
     /**
