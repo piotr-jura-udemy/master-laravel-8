5d21bdc556fbc48888b0b22bdef6073085de8470
Piotr Jura <>
2020-11-11 17:21:35 +0100

    #0058 Assets & Styling - Including assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <title>Laravel App - @yield('title')</title>
 </head>
 
