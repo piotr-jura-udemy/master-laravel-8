232e0f9aff6ec30c3ae2f210743fb56924a4e058
Piotr Jura <>
2020-11-11 20:32:36 +0100

    #0065 Assets & Styling - Styling single post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,23 +1,13 @@
 @extends('layouts.app')
 
-@section('title', $post['title'])
+@section('title', $post->title)
 
 @section('content')
-@if($post['is_new'])
-<div>A new blog post! Using if</div>
-@else
-<div>Blog post is old! using elseif/else</div>
+<h1>{{ $post->title }}</h1>
+<p>{{ $post->content }}</p>
+<p>Added {{ $post->created_at->diffForHumans() }}</p>
+
+@if(now()->diffInMinutes($post->created_at) < 5)
+<div class="alert alert-info">New!</div>
 @endif
-
-@unless($post['is_new'])
-<div>It is an old post... using unless</div>
-@endunless
-
-<h1>{{ $post['title'] }}</h1>
-<p>{{ $post['content'] }}</p>
-
-@isset($post['has_comments'])
-<div>The post has some comments... using isset</div>
-@endisset
-
 @endsection
