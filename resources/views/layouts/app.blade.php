007a057b8d4d3496d517f24ed2754a978961368e
Piotr Jura <>
2020-11-11 19:16:33 +0100

    #0062 Assets & Styling - Grid and Header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,16 @@
 </head>
 
 <body>
-    <div>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm mb-3">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Laravel App</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="{{ route('home.index') }}">Home</a>
+            <a class="p-2 text-dark" href="{{ route('home.contact') }}">Contact</a>
+            <a class="p-2 text-dark" href="{{ route('posts.index') }}">Blog Posts</a>
+            <a class="p-2 text-dark" href="{{ route('posts.create') }}">Add Blog Post</a>
+        </nav>
+    </div>
+    <div class="container">
         @if(session('status'))
             <div style="background: red;color:white">
                 {{ session('status') }}
