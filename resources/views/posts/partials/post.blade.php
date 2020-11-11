576d24369a586f4fb7f88f8661dae8a7967f5484
Piotr Jura <>
2020-11-11 20:20:23 +0100

    #0064 Assets & Styling - Styling post list page

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,15 +1,10 @@
-{{-- @break($key == 2) --}}
-{{-- @continue($key == 1) --}}
-@if($loop->even)
-<div>{{ $key }}.{{ $post->title }}</div>
-@else
-<div style="background-color: silver">{{ $key }}.{{ $post->title }}</div>
-@endif
+<h3><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a></h3>
 
-<div>
-    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+<div class="mb-3">
+    <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary">Edit</a>
+    <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
         @csrf
         @method('DELETE')
-        <input type="submit" value="Delete!">
+        <input type="submit" value="Delete!" class="btn btn-primary">
     </form>
 </div>
