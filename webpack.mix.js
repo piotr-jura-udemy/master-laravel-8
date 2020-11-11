664b777da79bf79b5a3ba768a9e6d8003db80334
Piotr Jura <>
2020-11-11 17:22:00 +0100

    #0059 Assets & Styling - Including versioned assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+if(mix.inProduction()) {
+  mix.version();
+}
